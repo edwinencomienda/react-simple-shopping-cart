5085435feb6d11c9504323226dd20ae0d4686d66
Edwin Encomienda <encomienda000@gmail.com>
2019-05-13 22:21:14 +0800

    responsive

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Cart from './componets/Cart';
 
 const App = () => {
   return (
-    <div className="w-full min-h-screen bg-grey-light flex flex-col items-center justify-center">
-      <h1 className="text-grey-darker">React Shopping Cart</h1>
-      <div className="flex mt-8" style={{ minWidth: 800 }}>
+    <div className="w-full min-h-screen bg-grey-light flex flex-col items-center">
+      <h1 className="text-grey-darker mt-10">React Shopping Cart</h1>
+      <div
+        className="lg:flex mt-8"
+        style={{ maxWidth: 800, width: '100%', padding: '0 15px' }}
+      >
         <Products />
         <Cart />
       </div>
